666c593daa9cc699b1221ff93657f8148d1c0eac
khushaalan <khushaalanarjunan@gmail.com>
2023-03-04 01:04:48 +0800

    rearrange

--- app.py
+++ app.py
@@ -7,28 +7,28 @@ app = Flask(__name__)
 # Home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('about.html', active_page='about')
 
 # Pages
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', active_page='about')
 
 @app.route('/skills')
 def skills():
-    return render_template('skills.html')
+    return render_template('skills.html', active_page='skills')
 
 @app.route('/workexperiences')
 def workexperiences():
-    return render_template('workexperiences.html')
+    return render_template('workexperiences.html', active_page='workexperiences')
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', active_page='projects')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', active_page='contact')
 
 
 
